d6f61d90b59fb569df7ecc55b4b516309c44fbad
darkstarshine2011 <150021527+darkstarshine2011@users.noreply.github.com>
2024-04-25 12:30:35 +0330

    Delete DSS SKILL directory

--- DSS SKILL/calculation/DSS-even-odd.py
+++
@@ -1,6 +0,0 @@
-print("_"*80, "\nNUM INFO (EVEN/ODD)")
-
-if num := float(input("number: "))%2 == 0: #it's my SKILL! "SKILL of darkstar.shine"
-    print("your number is Even")
-else:
-    print("your number is Odd")
--- DSS SKILL/start.bat
+++
@@ -1,2 +0,0 @@
-echo DSS SKILL v0.1 by: darkstarshine2011
-python DSS.py
